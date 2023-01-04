75f207fc9e25acf66152f90bc6f90d7cffb06961
Vitalii Danilov <Vitalii Danilov@DESKTOP-GIENFQV>
2023-01-04 21:08:42 +0700

    init: start task and prime program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let arrayNumberText = String(n).split('').map((n) => {
+        return String(n);
+    });
+
+    return Math.abs(Number(arrayNumberText.reverse().join().replace(/[,-]/gm,'')));
 }
